19deaef1c8ea148ebebf373d0971d0def4692b19
unknown <rogerio.junior@ab-inbev.com>
2023-02-13 13:11:32 -0300

    feat: add notes example

---
+++ proto/teste/Note.ts
@@ -0,0 +1,14 @@
+// Original file: proto/notes.proto
+
+
+export interface Note {
+  'id'?: (number);
+  'title'?: (string);
+  'description'?: (string);
+}
+
+export interface Note__Output {
+  'id': (number);
+  'title': (string);
+  'description': (string);
+}
