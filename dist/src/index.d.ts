c46bf69cbd56d8320d526bca35f6a108bd866d87
unknown <rogerio.junior@ab-inbev.com>
2023-02-23 10:23:42 -0300

    fix: generate types

--- dist/src/index.d.ts
+++ dist/src/index.d.ts
@@ -2,6 +2,7 @@ declare const Hermes: {
     addService: import("./application/config/protocols").IaddService;
     loadProto: import("./application/config/protocols").IaddService;
     start: import("./application/config/protocols").Istart;
+    client: import("./application/config/protocols").Iclient;
 };
 export { Hermes };
 //# sourceMappingURL=index.d.ts.map
--- dist/src/index.js
+++ dist/src/index.js
@@ -5,7 +5,8 @@ var controller_server_1 = require("./presentation/controller-server");
 var Hermes = {
     addService: controller_server_1.hermes.addService,
     loadProto: controller_server_1.hermes.addService,
-    start: controller_server_1.hermes.start
+    start: controller_server_1.hermes.start,
+    client: controller_server_1.hermes.client,
 };
 exports.Hermes = Hermes;
 //# sourceMappingURL=index.js.map
