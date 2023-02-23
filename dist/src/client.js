3389ac4915091b1732c3840c5d202011ee6ba19e
unknown <rogerio.junior@ab-inbev.com>
2023-02-23 09:38:08 -0300

    fix: remove client file

--- dist/src/client.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var grpc_1 = require("grpc");
-var gRPC_adapter_1 = require("./application/config/adapters/gRPC-adapter");
-gRPC_adapter_1.httpManager.client('127.0.0.1', '50051', grpc_1.credentials.createInsecure());
-//# sourceMappingURL=client.js.map
