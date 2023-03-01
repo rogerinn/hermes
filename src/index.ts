761f3e2a2780f05ed0c06150065bdd10d0a7fae6
unknown <rogerio.junior@ab-inbev.com>
2023-03-01 09:31:50 -0300

    fix: change loadproto in entry file

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { hermes } from './presentation/controller-server'
 
 const Hermes = {
     addService: hermes.addService,
-    loadProto: hermes.addService,
+    loadProto: hermes.loadProto,
     start: hermes.start,
     client: hermes.client,
 }
