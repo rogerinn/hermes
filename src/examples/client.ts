b21ed2ce1b7e1bd1b114f57ffea546e37e636a54
rogerinn <rogeriosandrojunior@hotmail.com>
2023-03-02 18:01:39 -0300

    refactor: change returns

--- src/examples/client.ts
+++ src/examples/client.ts
@@ -4,7 +4,6 @@ import { httpManager } from '../application/config/adapters/gRPC-adapter'
 const proto = httpManager.loadproto<ProtoGrpcType>(`${ __dirname }../../../proto/notes.proto`)
 const client = httpManager.client('127.0.0.1', 50051, proto.teste.Notes)
 
-client.List('', (error: any, response: any) => {
-    if(error) throw error
-    console.log(response)
-})
+const result = async () => console.log(await client.ListAsync({}))
+
+result()
